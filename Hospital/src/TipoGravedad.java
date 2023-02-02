1bd26122109c72f489d53845036ba2a87d89595a
EmmanuelRamosBiado <ramosbiadoe@gmail.com>
2023-02-02 12:55:54 +0100

    Carpetas borradas

--- Hospital/src/TipoGravedad.java
+++
@@ -1,4 +0,0 @@
-// Clase de tipo enumerado
-public enum TipoGravedad {
-	MENOS_GRAVE, GRAVE, MUY_GRAVE;
-}
--- Pizzería La Romántica/src/TipoPizza.java
+++
@@ -1,4 +0,0 @@
-
-public enum TipoPizza {
-	MARGARITA, CUATROQUESOS, BARBACOA, TROPICAL
-}
